965fd89c82a10fce367207b72c12bc340fbbc75f
15210280436 <65412597@qq.com>
2019-01-25 18:35:42 +0800

    红酒供应商聚类

--- k_means.R
+++ k_means.R
@@ -1,18 +1,86 @@
 library(tidyverse)
-library(nycflights13)
-library(ggplot2)
-library(DBI)
+library(ggthemes) # for plot theme
+library(janitor) # for clean name
+library(cluster) # for cluster algos
+library(gridExtra) # for putting multiple plots together
+library(factoextra) # extra help on clustering visualization
+library(DT) # for interactive table
 
+# set plot theme
+old <- theme_set(theme_tufte() + theme(text = element_text(family = "Menlo")))
 
-# raw data
-dat <- readr::read_rds("student_personas_lession.rds")
+# discount information
+offers <- read_csv("https://query.data.world/s/ixq45avtsmixmx4awbjuaueeaono7t") %>% clean_names()
+offers
 
-dat_1 <- dat %>% 
-  select(-student_id,-mobile) #去掉studentid以及mobile
+# orders information
+orders <- read_csv("https://query.data.world/s/hbzvqhg2iqtdhgzmi3k5jmvy3xeeph") %>% clean_names()
+orders
 
-dat_1
+# convert orders to matrix
+mm <- orders %>% 
+  mutate(purchase = 1) %>% 
+  spread(customer_last_name, purchase, fill = 0) %>% 
+  select(-offer_number) %>% 
+  data.matrix() 
 
-table(is.na(dat$total_catogery))# 判断空值列
+# set k = 2 
+km.2 <- kmeans(mm, centers = 2, iter.max = 50, nstart = 20)
 
-for (i=1 in 29)
+# view structure
+str(km.2)
 
+# visualise cluster
+offers %>% 
+  mutate(cluster = factor(km.2$cluster)) %>% 
+  ggplot(aes(origin, varietal, col = cluster)) + 
+  geom_point()
+
+# try different cluster numbers
+set.seed(1212)
+k <- 2:10
+km <- map(k, ~ kmeans(mm, centers = .x, iter.max = 50, nstart = 20))
+
+# collect plots using iteration
+display_cluster <- function(vec) {
+  offers %>% 
+    mutate(cluster = factor(vec)) %>% 
+    ggplot(aes(origin, varietal, col = factor(vec))) + 
+    geom_point() + 
+    scale_color_discrete(guide = 'none') + 
+    labs(x = "", y = "", col = "") + 
+    theme(text = element_text(size = 1))
+}
+p <- map(km, ~ display_cluster(.x$cluster))
+
+# open in Quartz, NOT in Rstudio 
+marrangeGrob(p, nrow = 3, ncol = 3)
+
+# the elbow method
+plot(k, map_dbl(km, "tot.withinss"), type = "b", 
+     frame = FALSE, 
+     xlab = "# Clusters, k",
+     ylab = "Total Within SS")
+
+# alternative
+fviz_nbclust(mm, kmeans, method = "wss")
+
+# extract silhouette and calc mean
+avg_sil <- function(k, dist_mat) {
+  ss <- silhouette(k, dist_mat)
+  mean(ss[, 3])
+}
+plot(k, map_dbl(km, ~ avg_sil(.x$cluster, dist(mm))), type = "b")
+
+# alternative
+fviz_nbclust(mm, kmeans, method = "silhouette")
+
+# use optimal clusters
+set.seed(1212)
+km.6 <- kmeans(mm, centers = 6, iter.max = 50, nstart = 20)
+
+# convert to DataTable
+offers %>% 
+  mutate(cluster = km.6$cluster) %>% 
+  DT::datatable(options = list(pageLength = 50, dom = 'tip'), 
+                filter = 'top')
